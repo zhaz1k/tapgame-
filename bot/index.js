d8e458a2666308d46517a28de8d31850233f554c
zhaz1k <sasha.stepanenko111@gmail.com>
2025-11-07 18:38:08 +0200

    Create index.js

---
+++ bot/index.js
@@ -0,0 +1,22 @@
+import TelegramBot from "node-telegram-bot-api";
+
+// 🔹 встав сюди свій токен з BotFather
+const TOKEN = "8421572582:AAE94DhUfhFsDoNRn0Vh7xxu5r79afD8Vzk";
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "🎮 Вітаю у Minecraft Tap Game!", {
+    reply_markup: {
+      keyboard: [
+        [
+          {
+            text: "⛏️ Грати зараз",
+            web_app: { url: "https://tapgame-phi.vercel.app" } // посилання на гру (Vercel)
+          }
+        ]
+      ],
+      resize_keyboard: true
+    }
+  });
+});
